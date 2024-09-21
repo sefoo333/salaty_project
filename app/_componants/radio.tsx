1675c927cb76e7f6c1b5e434b0ead319e2e486ae
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-21 15:49:16 +0300

    Update radio.tsx

--- app/_componants/radio.tsx
+++ app/_componants/radio.tsx
@@ -15,60 +15,67 @@ function Radio() {
     let [active2, setActive2] = useState(true)
     let [Time, setTime]: any = useState("")
     let audio2: any = useRef()
+
     return (
-        <div className="radio_window w-[700px] h-[500px] rounded-md bg-black relative">
+        <>
+            {typeof window !== "undefined" ? (
+                <div className="radio_window w-[700px] h-[500px] rounded-md bg-black relative">
 
 
 
-            <div className="radio_information absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col justify-center items-center">
-                <FaRadio className=' text-[#dddddd82] text-[50px]' />
-                <h1 className='text-[#ccc]'>{JSON.parse(localStorage.getItem("audio") || "").name2}</h1>
-            </div>
+                    <div className="radio_information absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col justify-center items-center">
+                        <FaRadio className=' text-[#dddddd82] text-[50px]' />
+                        <h1 className='text-[#ccc]'>{JSON.parse(localStorage.getItem("audio") || "").name2}</h1>
+                    </div>
 
-            <div className="bottom absolute bottom-0 bg-red-400 flex justify-between items-center w-full text-white py-[15px] px-[20px] text-[20px]">
-                <audio onTimeUpdate={(e) => {
-                    setTime(e.currentTarget.currentTime.toFixed(0))
+                    <div className="bottom absolute bottom-0 bg-red-400 flex justify-between items-center w-full text-white py-[15px] px-[20px] text-[20px]">
+                        <audio onTimeUpdate={(e) => {
+                            setTime(e.currentTarget.currentTime.toFixed(0))
 
 
-                }} ref={audio2} src={JSON.parse(localStorage.getItem("audio") || "").name}>
+                        }} ref={audio2} src={JSON.parse(localStorage.getItem("audio") || "").name}>
 
-                </audio>
-                <div className="play flex" onClick={() => {
-                    active2 ? setActive2(false)
-                        : setActive2(true)
-                }} >
+                        </audio>
+                        <div className="play flex" onClick={() => {
+                            active2 ? setActive2(false)
+                                : setActive2(true)
+                        }} >
 
-                    {active2 ? (
-                        <FaPlay className='mr-[20px] cursor-pointer' onClick={() => {
-                            if (audio2.current !== "") {
-                                audio2.current.play()
-                            }
-                        }} />
+                            {active2 ? (
+                                <FaPlay className='mr-[20px] cursor-pointer' onClick={() => {
+                                    if (audio2.current !== "") {
+                                        audio2.current.play()
+                                    }
+                                }} />
 
-                    ) : (
-                        <IoPause className='cursor-pointer' onClick={() => {
-                            if (audio2.current !== "") {
-                                audio2.current.pause()
-                            }
-                        }} />
-                    )}
+                            ) : (
+                                <IoPause className='cursor-pointer' onClick={() => {
+                                    if (audio2.current !== "") {
+                                        audio2.current.pause()
+                                    }
+                                }} />
+                            )}
 
+                        </div>
+                        <div className="audio_count flex">
+                            {!Number.isNaN((parseInt(Time) / 60)) ? (parseInt(Time) / 60).toFixed(0) : "0"}:{Time}
+                        </div>
+
+                        <div className="audio cursor-pointer" onClick={() => {
+                            active ? setActive(false) : setActive(true)
+                        }}>
+                            {active ? (<AiFillSound onClick={() => {
+                                audio2.current.volume = 0
+                            }} />) : (<FaVolumeMute onClick={() => {
+                                audio2.current.volume = 1
+                            }} />)}
+                        </div>
+                    </div>
                 </div>
-                <div className="audio_count flex">
-                    {!Number.isNaN((parseInt(Time) / 60)) ? (parseInt(Time) / 60).toFixed(0) : "0"}:{Time}
-                </div>
-
-                <div className="audio cursor-pointer" onClick={() => {
-                    active ? setActive(false) : setActive(true)
-                }}>
-                    {active ? (<AiFillSound onClick={() => {
-                        audio2.current.volume = 0
-                    }} />) : (<FaVolumeMute onClick={() => {
-                        audio2.current.volume = 1
-                    }} />)}
-                </div>
-            </div>
-        </div>
+            ) :
+                null
+            }
+        </>
     )
 }
 
