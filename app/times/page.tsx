19356d736050b058446b43c9b9e2725508ac8d92
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-21 15:33:36 +0300

    Update page.tsx

--- app/times/page.tsx
+++ app/times/page.tsx
@@ -14,14 +14,16 @@ function page() {
 
     return (
         <div className="parent px-[30px] w-full">
-            <Praytimes />
-
+{/*             <Praytimes />
+ */}
 
             <div className="pray_timenow mt-[50px]">
                 <div className="main mb-[10px] text-[40px] font-bold w-full flex justify-center px-[30px] py-[20px] text-nowrap text-center">
                     <h1 className="w-[200px] text-center border-b-[black] border-b-[5px]">الصلاة القادمة</h1>
                 </div>
-                <PrayNext />
+                
+{/*                 <PrayNext /> */}
+                
             </div>
         </div>
     )
