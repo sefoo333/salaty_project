279d3a9032f80ed0f18494a316603273e5a68131
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-21 14:09:09 +0300

    Update praytimesclient.tsx

--- app/_componants/praytimesclient.tsx
+++ app/_componants/praytimesclient.tsx
@@ -5,15 +5,15 @@ import React, { useEffect, useState } from 'react'
 function Praytimes(props: any) {
 
     let [data1, setGet]: any = useState([])
- let country: any = ""
-    if (typeof window !== "undefined") {
-        country = localStorage.getItem("country")
-    }
 
 
-    
+
+    let country: any = ""
     useEffect(() => {
         const get = async () => {
+            if (typeof window !== "undefined") {
+                country = localStorage.getItem("country")
+            }
             let response = await fetch(`http://api.aladhan.com/v1/timingsByCity?country=${JSON.parse(country).name[0]}&city=${JSON.parse(country).name[1]}`);
             let data1: any = await response.json()
             setGet(data1)
@@ -48,7 +48,7 @@ function Praytimes(props: any) {
     return (
         <div className="times">
             <div className="main text-[40px] font-bold w-full flex justify-end px-[30px] py-[20px]">
-                <h1 className="w-[200px] text-right border-b-[black] border-b-[5px]">{JSON.parse(country).name2[0]}</h1>
+                <h1 className="w-[200px] text-right border-b-[black] border-b-[5px]">{country !== "" ? JSON.parse(country).name2[0] : null}</h1>
             </div>
             <div className="elements grid grid-cols-3 w-full gap-[10px] mt-[30px] justify-items-center max-md:grid-cols-2 max-sm:grid-cols-1">
                 {salat.map((e) => (
