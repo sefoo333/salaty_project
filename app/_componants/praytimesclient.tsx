58da606442dff1e437c151256658bd6e3114ddfe
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-21 15:30:58 +0300

    Update praytimesclient.tsx

--- app/_componants/praytimesclient.tsx
+++ app/_componants/praytimesclient.tsx
@@ -1,71 +1,101 @@
 "use client"
+import { FiMenu } from "react-icons/fi";
 
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-
-function Praytimes(props: any) {
-
-    let [data1, setGet]: any = useState([])
-
-    let [country, setData2]: any = useState([]);
-
-
-
-
-
-    useEffect(() => {
-if (typeof window !== "undefined") {
-            setData2(JSON.parse(localStorage.getItem("country") || ''))
+interface props {
+    font: string
 }
-        const get = async () => {
-            let response = await fetch(`http://api.aladhan.com/v1/timingsByCity?country=${JSON.parse(localStorage.getItem("country") || '').name[0]}&city=${JSON.parse(localStorage.getItem("country") || '').name[1]}`);
-            let data1: any = await response.json()
-            setGet(data1)
 
+import { Rakkas } from "next/font/google"
 
-        }
-        return () => {
+const getCar = Rakkas({
+    subsets: ["latin"],
+    weight: ["400"],
+});
 
-            get();
-        }
-    }, [])
-
-    const salat = [{
-        name: "الفجر",
-        time: data1.data?.timings?.Fajr,
-    }, {
-        name: "الشروق",
-        time: data1.data?.timings?.Sunrise,
-    }, {
-        name: "الظهر",
-        time: data1.data?.timings?.Dhuhr,
-    }, {
-        name: "العصر",
-        time: data1.data?.timings?.Asr,
+const data = [
+    {
+        name: "الصفحـة الرئيسية",
+        href: "/"
     },
     {
-        name: "المغرب",
-        time: data1.data?.timings?.Maghrib,
-    },
-    {
-        name: "العشاء",
-        time: data1.data?.timings?.Isha,
+        name: "اوقــات الصلاة",
+        href: "/times"
+    }, {
+        name: "الأذكــار",
+        href: "/azkar"
+    }, {
+        name: "التسبيــح",
+        href: "/tsbeh"
+    }, {
+        name: "الراديــو",
+        href: "/radio"
+    }, {
+        name: "الإعدادات",
+        href: "/settings"
     }
-    ]
+]
+function Sidemenu() {
+    useEffect(() => {
+        if (JSON.parse(localStorage.getItem("darkmode") || "")) {
+            document.body.style.backgroundColor = "#3C3D37"
+            document.body.style.color = "white"
+        } else {
+            document.body.style.backgroundColor = "white"
+            document.body.style.color = "black"
+        }
+    }, [JSON.parse(localStorage.getItem("darkmode") || "")])
+    let [active, setActive] = useState(false)
+
     return (
-        <div className="times">
-            <div className="main text-[40px] font-bold w-full flex justify-end px-[30px] py-[20px]">
-                <h1 className="w-[200px] text-right border-b-[black] border-b-[5px]">{country.length !== 0 ? country.name2[0] : null}</h1>
-            </div>
-            <div className="elements grid grid-cols-3 w-full gap-[10px] mt-[30px] justify-items-center max-md:grid-cols-2 max-sm:grid-cols-1">
-                {salat.map((e) => (
-                    <div className="element px-[40px] py-[20px] bg-[#16423C] text-white rounded-xl text-[20px]">
-                        <h1>{e.name}</h1>
-                        <span>{parseInt(e.time?.slice(0, 2)) > 12 ? `0${parseInt(e.time.slice(0, 2)) - 12}:${e.time.slice(3, 5)}` : e.time}</span>
-                    </div>
-                ))}
-            </div>
+        <>
+            <FiMenu className="hidden z-[999999999] max-lg:block text-[30px] fixed left-[20px] top-[20px]" onClick={() => active ? setActive(false) : setActive(true)} />
+            <div className={`flex h-screen flex-col justify-between relative z-[99999999] border-e max-lg:h-full max-lg:w-full max-lg:items-center max-lg:justify-center max-lg:text-center max-lg:fixed bg-white ${!active ? `max-lg:hidden` : `max-lg:flex`}`} style={JSON.parse(localStorage.getItem("darkmode") || '') ? { backgroundColor: "#1E201E", color: "white" } : {}} >
+                <div className="px-4 py-6">
+                    <span className={`grid text-[35px] font-bold italic  h-10 w-32 place-content-center rounded-lg  text-white ${getCar.className}`}>
+                        صـلاتــي
+                    </span>
+
+                    <ul className="mt-6 space-y-1 text-nowrap">
+                        {data.map((e) => (
+                            <li>
+                                <Link
+                                    href={e.href}
+                                    className={`block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 ${!JSON.parse(localStorage.getItem("darkmode") || "") ? `hover:bg-gray-100 hover:text-gray-700` : `hover:bg-[#3C3D37] hover:text-white`}`}
+                                    style={JSON.parse(localStorage.getItem("darkmode") || '') ? { color: "white" } : {}}
+                                >
+                                    {e.name}
+                                </Link>
+                            </li>
+                        ))}
+
+
+
+                    </ul>
+                </div>
+
+                {/* <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
+      <a href="#" className="flex items-center gap-2 bg-white p-4 hover:bg-gray-50">
+        <img
+          alt=""
+          src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+          className="size-10 rounded-full object-cover"
+        />
+  
+        <div>
+          <p className="text-xs">
+            <strong className="block font-medium">Eric Frusciante</strong>
+  
+            <span> eric@frusciante.com </span>
+          </p>
         </div>
+      </a>
+    </div> */}
+            </div>
+        </>
     )
 }
 
-export default Praytimes
+export default Sidemenu
