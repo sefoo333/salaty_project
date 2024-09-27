28e2ad20ca2bb71ebf7201275fcc96c9e4f3483d
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-27 09:01:33 +0300

    Update request.tsx

--- app/_componants/request.tsx
+++ app/_componants/request.tsx
@@ -1,19 +1,19 @@
 "use client"
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 
 function Request() {
       useEffect(() => {   
-            if (typeof window !== "undefined") {
         localStorage.setItem("darkmode", JSON.stringify(false))
         localStorage.setItem("coutry", JSON.stringify({
             name: ["EG", "cairo", "مصر"],
             name2: ["مصر"]
         }))
+    
     }, [])
 
 
-    }
+    
 
     return (
         <></>
