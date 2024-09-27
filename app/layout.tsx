b5d1a61e91cd45659f5148fe1817afffdcdcb973
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-27 07:31:05 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,6 +45,9 @@ export default function RootLayout({
 
   return (
     <html lang="ar">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
        <body
         className={`${geistSans.variable} ${geistMono.className} antialiased`}
       >
