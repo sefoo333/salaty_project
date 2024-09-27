f0a9e16d0f2b5600713e99a845ca3c4b4be6b9e4
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-27 12:11:38 +0300

    Update footer.tsx

--- app/_componants/footer.tsx
+++ app/_componants/footer.tsx
@@ -30,16 +30,27 @@ function Footer() {
         <>
                 <>
                     {!dark2 ? (
-                        <footer className="bg-gray-50">
+                         <footer className="bg-gray-50">
                             <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
                                 <div className="sm:flex sm:items-center sm:justify-between">
                                     <div className={`flex justify-center text-teal-600 sm:justify-start ${getCar.className}`}>
                                         <h1>صلاتـي</h1>
                                     </div>
 
-                                    <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
-                                        Copyright &copy; {new Date().getFullYear()}. All rights reserved.
-                                    </p>
+                                    <div className="group">
+                                            <p className="mt-4 text-center text-sm lg:mt-0 lg:text-right text-gray-400">
+                                                Copyright &copy; {new Date().getFullYear()}. All rights reserved.
+                                            </p>
+                                            <div className="links flex text-[20px] mt-[10px] items-center justify-center">
+                                                <a href="https://www.linkedin.com/in/sefoo333/" target='_blank' className='transition p-[10px] hover:bg-slate-800 rounded-full'>
+                                                    <FaGithub  className='text-black' />
+                                                </a>
+                                                <a href="https://github.com/sefoo333" target="_blank" className='ml-[10px]  p-[10px] transition hover:bg-cyan-500 rounded-full'>
+                                                    <IoLogoLinkedin  className='text-black'/>
+                                                </a>
+
+                                            </div>
+                                        </div>
                                 </div>
                             </div>
                         </footer>
