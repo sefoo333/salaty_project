fcb2a9696304c2dd2c5a191718cc7599ca52c15e
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-27 08:20:42 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,15 +32,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  if(typeof window !== "undefined"){
-                localStorage.setItem("darkmode", JSON.stringify(false))
-                localStorage.setItem("coutry", JSON.stringify({
-                    name: ["EG", "cairo", "مصر"],
-                    name2: ["مصر"]
-                }))
-            
-    
-    }
+
 
   return (
     <html lang="ar">
