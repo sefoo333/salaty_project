4b0f8470bf46f10b5c3c8486a6725af99de26eac
sefoo333 <111438242+sefoo333@users.noreply.github.com>
2024-09-27 09:49:34 +0300

    Update request.tsx

--- app/_componants/request.tsx
+++ app/_componants/request.tsx
@@ -4,11 +4,13 @@ import React, { useEffect } from 'react'
 
 function Request() {
       useEffect(() => {   
-        localStorage.setItem("darkmode", JSON.stringify(false))
+       if (localStorage.length == 0){
+              localStorage.setItem("darkmode", JSON.stringify(false))
         localStorage.setItem("coutry", JSON.stringify({
             name: ["EG", "cairo", "مصر"],
             name2: ["مصر"]
         }))
+       }
     
     }, [])
 
